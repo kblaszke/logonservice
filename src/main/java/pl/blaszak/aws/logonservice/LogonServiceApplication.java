2e4aabf5a81c38b6c4462fca0fa88383bec487c0
Blaszke Krzysztof <krzysztof.blaszke.ctr@sabre.com>
2019-03-05 11:43:15 +0100

    first commit

---
+++ src/main/java/pl/blaszak/aws/logonservice/LogonServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.blaszak.aws.logonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/blaszak/aws/logonservice/LogonServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.blaszak.aws.logonservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogonServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
