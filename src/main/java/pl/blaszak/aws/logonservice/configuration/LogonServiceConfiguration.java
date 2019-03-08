d63b33985dd9682b43d18b2549d626952f4cc93b
Blaszke Krzysztof <krzysztof.blaszke.ctr@sabre.com>
2019-03-08 16:12:27 +0100

    [feature/javaClassConfiguration] changed configuration from properties to java Class

--- src/main/java/pl/blaszak/aws/logonservice/configuration/LogonServiceConfiguration.java
+++ src/main/java/pl/blaszak/aws/logonservice/configuration/LogonServiceConfiguration.java
@@ -2,15 +2,33 @@ package pl.blaszak.aws.logonservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 import pl.blaszak.aws.logonservice.couchbase.repository.UserRepository;
 import pl.blaszak.aws.logonservice.service.UserService;
 
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
-public class LogonServiceConfiguration {
+public class LogonServiceConfiguration extends AbstractCouchbaseConfiguration {
 
     @Bean
     public UserService getCouchbaseUserService(UserRepository userRepository) {
         return new UserService(userRepository);
     }
 
+    @Override
+    protected List<String> getBootstrapHosts() {
+        return Collections.singletonList("localhost");
+    }
+
+    @Override
+    protected String getBucketName() {
+        return "logonServiceBucket";
+    }
+
+    @Override
+    protected String getBucketPassword() {
+        return "Wiosna2019";
+    }
 }
--- src/main/java/pl/blaszak/aws/logonservice/couchbase/repository/UserRepository.java
+++ src/main/java/pl/blaszak/aws/logonservice/couchbase/repository/UserRepository.java
@@ -2,12 +2,10 @@ package pl.blaszak.aws.logonservice.couchbase.repository;
 
 import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
 import org.springframework.data.couchbase.core.query.ViewIndexed;
-import org.springframework.data.couchbase.repository.CouchbasePagingAndSortingRepository;
+import org.springframework.data.couchbase.repository.CouchbaseRepository;
 import pl.blaszak.aws.logonservice.couchbase.model.User;
 
 @N1qlPrimaryIndexed
 @ViewIndexed(designDoc = "user")
-public interface UserRepository extends CouchbasePagingAndSortingRepository<User, String> {
-
-
+public interface UserRepository extends CouchbaseRepository<User, String> {
 }
